506872347acb64f82edf1b1f1063de6a6b02fbf6
mcpengnetwork <83247845+mcpengnetwork@users.noreply.github.com>
2021-05-18 17:40:54 -0400

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,1 +1,7 @@
 <?php
+  $dbServername = "id16845796_spamdatabase";
+  $dbUsername = "id16845796_spam";
+  $dbPassword = "3z=Y|siXg~0#cx=E";
+  $dbName = "database";
+  
+  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
